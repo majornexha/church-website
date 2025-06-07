984c0b7e53e1f6254512d4642d851987062a73b0
majornexha <naleerabarisudorwilson@gmail.com>
2025-06-06 19:07:21 -0500

    Update

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('nav-links');
-    const toggleBtn = document.getElementById('darkToggle');
 
-    // Hamburger toggle
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('show');
     });
-
-    // Dark mode toggle
-    toggleBtn.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
-    });
-
-    // Remove preloader after page load
-    window.onload = () => {
-        const loader = document.getElementById('preloader');
-        loader.style.display = 'none';
-    };
 });
